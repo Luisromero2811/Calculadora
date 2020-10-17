49e37f4cc2b5f66a89912cb57de5cb1c37098a0a
Luis Romero <19170067@uttcampus.edu.mx>
2020-10-17 16:52:59 -0500

    Subi calculadora funcionando al cien y borre documentacion

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     TextView Res;
     double res;
     String op, showv, reser;
-//Code
+
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
